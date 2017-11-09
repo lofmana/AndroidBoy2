77c047342613ab9ec2b8881446b517fe0805b7f0
lofmana <lofmana@hotmail.com>
2017-11-09 20:58:18 +0800

    nth
    nth

--- app/src/main/java/com/example/mdpgroup9/androidboy/RepeatListener.java
+++ app/src/main/java/com/example/mdpgroup9/androidboy/RepeatListener.java
@@ -39,13 +39,6 @@ public class RepeatListener implements OnTouchListener{
     private View downView;
 
 
-    /**
-     * @param initialInterval The interval after first click event
-     * @param normalInterval The interval after second and subsequent click
-     *       events
-     * @param clickListener The OnClickListener, that will be called
-     *       periodically
-     */
     public RepeatListener(int initialInterval, int normalInterval,
                           OnClickListener clickListener , Activity _activity) {
         if (clickListener == null)
